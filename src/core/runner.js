4aeb2781d33a5e78089800765813fdf0586e59b5
qiuzhch <apchy!@>
2020-06-28 21:45:37 +0800

    update

--- src/core/runner.js
+++ src/core/runner.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import cb from './cube';
 
+import { isArray } from '../util';
+
 class MetaRunner extends Component {
     constructor(props) {
         super(props);
@@ -17,13 +19,19 @@ class MetaRunner extends Component {
     }
 
     parseContainers() {
+        const { viewmeta } = this.state;
+        if (isArray(viewmeta.containers)) {
+            viewmeta.containers.foreach(container => {
+                
+            })
+        }
         return <div>container.</div>
     }
 
     render() {
         if (!this.state.viewmeta) return '解析中...';
         return <div>
-            { this.parseContainers(this.state.viewmeta) }
+            { this.parseContainers() }
         </div>
     }
 }
