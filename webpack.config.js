c66092590e084c9e9c797ac32924ce878523e33e
qiuzhch <apchy!@>
2020-06-28 20:48:53 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './index.html'
     }),
     new CleanWebpackPlugin({}),
     new webpack.HotModuleReplacementPlugin()
